36bc54cc4f4115ff535ac7fdee07ecd448400c8a
Brian Farnsworth <farnswor@adobe.com>
2018-02-25 15:30:47 -0700

    more style

--- Lunch/App.js
+++ Lunch/App.js
@@ -2,10 +2,25 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>LUNCH?</Text>
+        <View style={styles.partialBackground}></View>
+        <Text style={[styles.welcomeMessage, styles.text]}>Welcome to</Text>
+        <Text style={[styles.text]}>Lunch</Text>
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+        <Text>Take the guesswork out of catching up over a bite to eat.</Text>
+        <Text></Text>
+        <Text></Text>
+        <Text>Some graphic</Text>
+        <Text>....</Text>
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+        <Text>SIGN UP</Text>
       </View>
     );
   }
@@ -16,6 +31,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 30
   },
+  text: {
+    color: '#2799fb',
+    fontSize: 30,
+  },
+  partialBackground: {
+    backgroundColor: '#f1f9ff',
+    position: 'absolute',
+    height: '60%',
+    width: '200%',
+  },
+  welcomeMessage: {
+    marginTop: '20%'
+  },
+
 });
