fec7e80f6b8ae3b68495eb40a77866e9a7175bbe
Brian Farnsworth <farnswor@adobe.com>
2018-03-10 15:15:09 -0700

    Got new styles started and got fonts loading

--- Lunch/App.js
+++ Lunch/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Font } from 'expo';
+
 
 
 const BLUE_1 = '#2799fb';//main darker font color
@@ -12,24 +14,65 @@ export default class App extends React.Component {
 
   constructor(...args) {
     super(...args);
-    this.state = {loggedIn: false};
+    this.state = {
+      loggedIn: false,
+      fontsLoaded: false,
+      page: 'splashScreen'
+    };
+  }
+
+  componentDidMount() {
+
+    //load fonts
+    Font.loadAsync({
+      'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+    }).then(() => {
+      this.setState({
+        fontsLoaded: true
+      })
+    });         
+
+
+    //set fake timeout for splash screen
+    setTimeout(() => {
+      this.setState({
+        page: 'onboarding'
+      });
+    }, 3000);
+
   }
 
   onSignUpPress() {
     this.setState({loggedIn: true});
   }
 
-  render() {
+  renderSplashScreen() {
 
-    if(this.state.loggedIn) {
-      return (
-        <View style={styles.container}>
-          <Text style={{fontSize: 20, color: BLUE_1}}>Logged In</Text>
-          <TouchableOpacity onPress={() => this.setState({loggedIn: false})}><Text>Back</Text></TouchableOpacity>
-        </View>
-      )
+    const iconStyle = {
+      backgroundColor: 'white',
+      borderRadius: 50,
+      height: 50,
+      width: 50,
     }
 
+    const titleStyle = {
+      fontSize: 60,
+      color: '#F23838',
+      fontFamily: 'Roboto-Medium'
+    }
+
+    return (
+      <View style={spashScreenStyles.container}>
+        <View style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+          <View style={iconStyle}></View>
+          {this.state.fontsLoaded ? <Text style={titleStyle}>Lunch</Text> : null}
+        </View>
+      </View>
+    );
+  }
+
+
+  renderOnBoardingPage() {
     return (
       <View style={styles.container}>
         <View style={styles.partialBackground}></View>
@@ -50,6 +93,25 @@ export default class App extends React.Component {
         </TouchableOpacity>
       </View>
     );
+  }
+
+
+  render() {
+
+    if(this.state.page === 'splashScreen') {
+      return this.renderSplashScreen();
+    } else if(this.state.page === 'onboarding') {
+      return this.renderOnBoardingPage();
+    }
+
+    // if(this.state.loggedIn) {
+    //   return (
+    //     <View style={styles.container}>
+    //       <Text style={{fontSize: 20, color: BLUE_1}}>Logged In</Text>
+    //       <TouchableOpacity onPress={() => this.setState({loggedIn: false})}><Text>Back</Text></TouchableOpacity>
+    //     </View>
+    //   )
+    // }
   }
 }
 
@@ -72,6 +134,17 @@ const Graphic = () => {
     </View>
   )
 }
+
+const spashScreenStyles = StyleSheet.create({
+  container: {
+    backgroundColor: '#2A2E43',
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
